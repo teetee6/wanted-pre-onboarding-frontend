8c8670536da830267d6af2750c318c174649aeea
teetee6 <teetee6@naver.com>
2023-08-11 20:56:26 +0900

    assignment6/feat: Implemented input and button for adding new TODOs on the list page

--- src/pages/TodoList/TodoList.jsx
+++ src/pages/TodoList/TodoList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './TodoList.css'; // Import your CSS file
 
@@ -15,11 +15,35 @@ function TodoItem({ text, completed }) {
 
 function TodoList() {
   const navigate = useNavigate();
-  const todos = [
-    { id: 1, text: 'TODO 1', completed: false },
-    { id: 2, text: 'TODO 2', completed: true },
-    { id: 3, text: 'TODO 3', completed: false },
-  ];
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = async () => {
+    if (newTodo.trim() !== '') {
+      try {
+        const response = await fetch('/todos', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+          },
+          body: JSON.stringify({
+            todo: newTodo,
+          }),
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setTodos([...todos, data]);
+          setNewTodo('');
+        } else {
+          console.error('Failed to add new TODO');
+        }
+      } catch (error) {
+        console.error('Error during TODO creation:', error);
+      }
+    }
+  };
 
   useEffect(() => {
     const accessToken = localStorage.getItem('access_token');
@@ -29,11 +53,53 @@ function TodoList() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    async function fetchTodos() {
+      try {
+        const response = await fetch('/todos', {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setTodos(data);
+        } else {
+          console.error('Failed to fetch TODOs');
+        }
+      } catch (error) {
+        console.error('Error during TODO fetching:', error);
+      }
+    }
+
+    fetchTodos();
+  }, []);
+
   return (
     <ul className="todo-list">
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} text={todo.text} completed={todo.completed} />
-      ))}
+      <div className="input-container">
+        <input
+          data-testid="new-todo-input"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button data-testid="new-todo-add-button" onClick={handleAddTodo}>
+          추가
+        </button>
+      </div>
+      {todos.map((todo) => {
+        // console.log(todo);
+        // todo.userId 활용해서 수정,삭제 권한 부여
+        return (
+          <TodoItem
+            key={todo.id}
+            text={todo.todo}
+            completed={todo.isCompleted}
+          />
+        );
+      })}
     </ul>
   );
 }
